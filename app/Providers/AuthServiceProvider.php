0a5f3bd6e14932976e3af7a2f81468f99882cf96
michael <michael8e.php@gmail.com>
2025-05-16 18:57:01 +0300

    first commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Video;
+use App\Policies\VideoPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Channel::class => ChannelPolicy::class,
+        Video::class => VideoPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
